724029cee838c90f892f24abeaa81205ee2b5c4b
sky3691841 <sky3691841@gmail.com>
2020-03-11 20:40:06 +0800

    handle register and login

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
     mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
     Campground = require("./models/campground"),
     Comment = require("./models/comment"),
+    User = require("./models/user"),
     seedDB = require("./seeds")
 
 seedDB();
@@ -14,6 +17,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Rusty is a little chubby",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -95,6 +110,42 @@ app.post("/campgrounds/:id/comments", function(req, res) {
             })
         }
     });
+});
+
+// ================
+// AUTH ROUTES
+// ================
+// show register form
+app.get("/register", function(req, res) {
+    res.render("register");
+})
+
+// add sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/campgrounds");
+        });
+    })
+})
+
+// show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+// handling login logic
+app.post("/login", passport.authenticate("local",
+{
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req, res) {
+
 });
 
 app.listen(port, function() {
