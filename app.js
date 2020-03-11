f06217b374841cac8d6ae96374315e593f125160
sky3691841 <sky3691841@gmail.com>
2020-03-11 16:50:00 +0800

    added mongoose

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 var port = process.env.PORT || 3000;
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
+mongoose.connect("mongodb://localhost/yelp_camp", {
+    useNewUrlParser:true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "North Coast", image: "https://photo.travelking.com.tw/scenery/6BADB9CF-D593-493E-9AAF-556E4B4A73AD_e.jpg"},
-    {name: "Yangming Mountain", image: "https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/10/04/draft/6899908.jpg&x=0&y=0&sw=0&sh=0&sl=W&fw=800&exp=3600&w=930"}
-]
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+// Campground.create(
+//     {
+//         name: "Yangming Mountain",
+//         image: "https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/10/04/draft/6899908.jpg&x=0&y=0&sw=0&sh=0&sl=W&fw=800&exp=3600&w=930"
+//     }, 
+//     function(err, campground) {
+//         if(err) {
+//             console.log(err);
+//         } else {
+//             console.log("Newly Created Campground");
+//             console.log(campground);
+//         }
+//     })
 
 app.get("/", function(req, res) {
     res.render("landing");
@@ -17,18 +36,31 @@ app.get("/", function(req, res) {
 
 /** Show all the campgrounds */
 app.get("/campgrounds", function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds});
-})
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, allcampgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allcampgrounds});
+        }
+    });
+});
 
 /** Logic when submit button is hit */
 app.post("/campgrounds", function(req, res) {
     var name = req.body.name
     var image = req.body.image
     var newCampground = {name:name, image:image}
-    campgrounds.push(newCampground);
-
-    res.redirect("/campgrounds");
-})
+    
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
 
 /** Shows the form to create a new campground */ 
 app.get("/campgrounds/new", function(req, res) {
