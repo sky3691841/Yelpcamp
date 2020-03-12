f434f266ed38ea6806f2f243ccd5181bdfc51946
sky3691841 <tyhuang06@gmail.com>
2020-03-12 10:14:11 +0800

    added logout route

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get("/campgrounds/:id", function(req, res) {
 // COMMENTS ROUTES
 // =============================
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn , function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -93,7 +93,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
     })
 });
 
-app.post("/campgrounds/:id/comments", function(req, res) {
+app.post("/campgrounds/:id/comments", isLoggedIn ,function(req, res) {
     Campground.findById(req.params.id, function(err, campground) {
         if (err) {
             console.log(err);
@@ -145,8 +145,20 @@ app.post("/login", passport.authenticate("local",
     successRedirect: "/campgrounds",
     failureRedirect: "/login"
 }), function(req, res) {
-
 });
+
+// logout route
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+};
 
 app.listen(port, function() {
     console.log("The Yelpcamp Server has started!");
