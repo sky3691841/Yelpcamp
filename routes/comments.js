1ec476a6e62251130b795fb7a6b7b514bf1b736f
sky3691841 <tyhuang06@gmail.com>
2020-03-12 14:35:04 +0800

    added comment authentication

--- routes/comments.js
+++ routes/comments.js
@@ -41,7 +41,7 @@ router.post("/", isLoggedIn ,function(req, res) {
 });
 
 // Comments Edit
-router.get("/:comment_id/edit", function(req, res) {
+router.get("/:comment_id/edit", checkCommentOwnership,function(req, res) {
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if (err) {
             res.redirect("back");
@@ -52,7 +52,7 @@ router.get("/:comment_id/edit", function(req, res) {
 });
 
 // Comments Update
-router.put("/:comment_id", function(req, res) {
+router.put("/:comment_id", checkCommentOwnership,function(req, res) {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
         if (err) {
             res.redirect("back");
@@ -63,7 +63,7 @@ router.put("/:comment_id", function(req, res) {
 });
 
 // Comments Destroy
-router.delete("/:comment_id", function(req, res) {
+router.delete("/:comment_id", checkCommentOwnership,function(req, res) {
     Comment.findByIdAndRemove(req.params.comment_id, function(err) {
         if (err) {
             res.redirect("back");
@@ -80,5 +80,24 @@ function isLoggedIn(req, res, next) {
     }
     res.redirect("/login");
 };
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, function(err, foundComment) {
+            if (err) {
+                res.redirect("back");
+            } else {
+                // check if user owns the comment
+                if(foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
 
 module.exports = router;
